a620d1a08877a3bfc8e1117d038f9b9ea3f4229c
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2018-04-19 17:51:45 +0200

    Let's not.

--- static/ts/main.ts
+++ static/ts/main.ts
@@ -1,13 +1,5 @@
 // Update the random text below the logo.
 
-function isDoNotTrack(): boolean {
-	if (!navigator.doNotTrack) {
-		return false;
-	}
-
-	return navigator.doNotTrack === "1" || navigator.doNotTrack === "yes";
-}
-
 /**
  * Picks a random element from an array.
  * @param array The array to pick from.
@@ -16,42 +8,16 @@ function pick<T>(array: T[]): T {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
-function initgtag() {
-	window.dataLayer = window.dataLayer || [];
-	gtag('js', new Date());
-	gtag('config', 'UA-113656108-1');
-
-	var s = document.createElement("script");
-	s.src = "https://www.googletagmanager.com/gtag/js?id=UA-113656108-1";
-	s.async = true;
-	document.head.appendChild(s);
-}
-
-function gtag(...args: any[]) {
-	window.dataLayer.push(arguments);
-}
-
-interface Window {
-	dataLayer: any[];
-}
-
 document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById("subname").innerHTML = pick([
 		"Crashing SS13, with no survivors!",
 		"Get dat fukken remake!",
 		"Soon&trade;",
 		"Beat up clowns in HD!",
-		"Brought to you by <s>ROBUSTENGINE&trade;</s>SFML",
+		"Brought to you by <s>ROBUSTENGINE&trade;</s> <s>SFML</s> Godot Game Engine",
 		"Suprisingly not dead!",
 		"We're at it again!",
 		"Now with 20% more space!",
-		"Honking 24/7!",
-		"Waiting for Godot"
+		"Honking 24/7!"
 	]) + "<br />";
-
-	if (!isDoNotTrack()) {
-		// Add Google analytics ONLY if DNT is disabled.
-		// Respect user's choice please.
-		initgtag();
-	}
 });
