cdfb17e619831875e6d7b704a8fb2ce68dc324e9
ike709 <ike709@github.com>
2022-11-26 15:10:03 -0700

    oops nuke the copy of the script

--- static/images/post/pr_38/pr-image-convert.ps1
+++
@@ -1,7 +0,0 @@
-function losslesswebp($filename) {
-	optipng -o5 $filename
-    $webpPath = [System.IO.Path]::ChangeExtension($filename, "webp")
-    magick $filename -define webp:lossless=true $webpPath
-}
-
-gci *.png | % { losslesswebp($_); }
